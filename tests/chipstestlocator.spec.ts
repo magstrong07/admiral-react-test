7dec1b1d65c5d46784f98989cb3f39b671d19559
Andrey Moskvin <moskowkach@gmail.com>
2022-06-06 18:59:23 +0300

    delet experiment test

--- tests/chipstestlocator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('test', async ({ page }) => {
-  // Go to https://magstrong07.github.io/admiral-react-test/#/chips
-  await page.goto('https://magstrong07.github.io/admiral-react-test/#/chips');
-  // Click .sc-dFdIVH >> nth=0
-  await page.locator('.sc-dFdIVH').first().click();
-});
