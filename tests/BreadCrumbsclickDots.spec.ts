6ea5aa32fe9243e94b72acb196bae61adedef15e
Andrey Moskvin <moskowkach@gmail.com>
2022-06-07 12:08:23 +0300

    chips snapshots

--- tests/BreadCrumbsclickDots.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  // Go to http://localhost:6006/?path=/story/admiral-2-1-breadcrumbs--sizes
-  await page.goto('http://localhost:6006/?path=/story/admiral-2-1-breadcrumbs--sizes');
-
-  // Click text=page 1page 3page 4current page >> button
-  await page.frameLocator('#storybook-preview-iframe').locator('text=page 1page 3page 4current page >> button').click();
-});
