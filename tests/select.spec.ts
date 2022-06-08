7815c1dd8f3ccb0bb380e3967b6db4ca254f8837
Andrey Moskvin <moskowkach@gmail.com>
2022-06-08 13:46:32 +0300

    select fix (wait update react-ui)

--- tests/select.spec.ts
+++ tests/select.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect } from '@playwright/test';
 test.describe('select', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://magstrong07.github.io/admiral-react-test/#/select');
+    await page.goto('http://localhost:3000/#/select');
   });
   test('select', async ({ page }) => {
-    await page.click('div[role="button"]:has-text("Placeholder")');
-    await page.click('text=Option three');
-    await page.click('div[role="button"]:has-text("Option one")');
-    await page.click('li[role="option"]:has-text("Option six")');
-    await page.click('div[role="button"]:has-text("Номер Карты /****45")');
-    await page.click('text=Номер Карты /****35 Дополнительный текст');
-    await page.click('text=Option threeOption sixНомер Карты /****35 >> :nth-match(div[role="button"], 4)');
-    await page.click('text=Option three long text long text');
-    await page.waitForTimeout(350);
+    await page.waitForTimeout(200);
+    await page.locator('select >> nth=0').click();
+    await page.locator('text=Option three').click();
+
     await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
   });
 });
