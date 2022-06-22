dc96aa8e62cf2d0c0459d808540aebc233c3fa01
Andrey Moskvin <moskowkach@gmail.com>
2022-06-22 18:01:26 +0300

    new snapshots

---
+++ tests/SearchSelectFied.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+test.describe('search select field', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://magstrong07.github.io/admiral-react-test/#/search_select_field');
+  });
+  test('search select field click', async ({ page }) => {
+    await page.locator('input >> nth=0').click();
+    await page.locator('#selectDropdownContainer div:has-text("teeext 1")').first().click();
+    await page.locator('input >> nth=1').click();
+    await page.locator('div:has-text("Текст 1Доооп Текст 1")').nth(2).click();
+    await page.locator('input >> nth=4').click();
+    await page.locator('#selectDropdownContainer div:has-text("texttt 6")').first().click();
+    await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
+  });
+  test('search select field input text', async ({ page }) => {
+    await page.locator('input >> nth=0').click();
+    await page.locator('input >> nth=0').fill('teeext');
+    await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
+  });
+  test('search select field input text 2', async ({ page }) => {
+    await page.locator('input >> nth=1').click();
+    // await page.keyboard.type('Доп');
+    await page.locator('input >> nth=1').fill('Доп');
+
+    await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
+  });
+});
