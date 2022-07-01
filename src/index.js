e0f6a1ca3a9b5d610d7f6f786c6b7b5603ef29d2
Andrey Moskvin <moskowkach@gmail.com>
2022-07-01 14:50:35 +0300

    fonts fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
 import App from './App.jsx';
 import './App.css';
-import { LIGHT_THEME, FontsVTBGroup } from '@admiral-ds/react-ui';
+import { LIGHT_THEME } from '@admiral-ds/react-ui';
 import { ThemeProvider } from 'styled-components';
 import '@admiral-ds/fonts/VTBGroupUI.css';
 
 ReactDom.render(
   <React.StrictMode>
     <ThemeProvider theme={LIGHT_THEME}>
-      {/* <FontsVTBGroup /> */}
       <App />
     </ThemeProvider>
   </React.StrictMode>,
