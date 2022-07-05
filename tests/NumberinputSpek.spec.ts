6d11495d887c2a00b10f560afaf36256b0f86fa3
Andrey Moskvin <moskowkach@gmail.com>
2022-07-05 11:50:57 +0300

    reload input test

--- tests/NumberinputSpek.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('numbers111', async ({ page }) => {
-  await page.goto('https://magstrong07.github.io/admiral-react-test/#/number_input');
-  await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
-});
