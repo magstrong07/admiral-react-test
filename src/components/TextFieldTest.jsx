2d2f1bd4934dfe89cffa06940faab1463412c623
Andrey Moskvin <moskowkach@gmail.com>
2022-07-20 17:54:40 +0300

    TextField fix

--- src/components/TextFieldTest.jsx
+++ src/components/TextFieldTest.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { TextField, Hint, HintDialog, T } from '@admiral-ds/react-ui';
+import { TextField, T } from '@admiral-ds/react-ui';
 import styled from 'styled-components';
-import { ReactComponent as HelpSolidSVG } from '@admiral-ds/icons/build/service/HelpSolid.svg';
 
 //TODO доделать
 const Wrapper = styled.div`
@@ -34,53 +33,19 @@ function func2(dimension, status) {
   });
 }
 dimensions.forEach((d) => func1(d));
-const HelpSolid = styled(HelpSolidSVG)`
-  & *[fill^='#'] {
-    fill: ${(p) => p.theme.color.text.secondary};
-  }
-  [data-focus-within] & *[fill^='#'] {
-    fill: ${(props) => props.theme.color.basic.hover};
-  }
-`;
-
-const InverseColor = styled(T)`
-  color: ${(p) => p.theme.color.text.staticWhite};
-`;
-
-const InverseBackgroundHint = styled(Hint)`
-  & ${HintDialog} {
-    background-color: ${(p) => p.theme.color.background.inversion};
-  }
-`;
 
 const TextFields = (props) => {
-  const [localValue, setValue] = React.useState(props.value);
+  const [localValue, setValue] = React.useState('');
   const fieldRef = React.useRef(null);
   const handleChange = (e) => {
     const inputValue = e.target.value;
     setValue(inputValue);
-    props.onChange?.(e);
   };
   return (
     <>
       <TextField {...props} value={localValue} onChange={handleChange} />
-      <TextField required label="Поле необходимо заполнить" />
-      <TextField
-        ref={fieldRef}
-        label="Напишите сочинение на заданную тему"
-        icons={
-          <InverseBackgroundHint
-            target={fieldRef}
-            renderContent={() => (
-              <InverseColor as="span" font="Additional/S">
-                Тема сочинения: Как я использую компоненты?
-              </InverseColor>
-            )}
-          >
-            <HelpSolid />
-          </InverseBackgroundHint>
-        }
-      />
+      <TextField {...props} required label="Поле необходимо заполнить" />
+      <TextField {...props} ref={fieldRef} label="Напишите сочинение на заданную тему" />
     </>
   );
 };
