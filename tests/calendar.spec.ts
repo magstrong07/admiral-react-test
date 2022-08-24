f4b8fc3397f938f92d0b3419553a5fbfa5fbbea4
Andrey Moskvin <moskowkach@gmail.com>
2022-08-24 14:47:49 +0300

    calendar click

--- tests/calendar.spec.ts
+++ tests/calendar.spec.ts
@@ -1,8 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('calendar click', async ({ page }) => {
   await page.goto('https://magstrong07.github.io/admiral-react-test/#/calendar');
 
   await page.locator('[data-test-id="calendar1"] svg').nth(1).click();
-  await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
 });
