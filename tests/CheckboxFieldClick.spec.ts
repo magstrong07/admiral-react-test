f939a753cb5150ec5ab4b61eed2ec2b5279ac471
Andrey Moskvin <moskowkach@gmail.com>
2022-10-11 18:28:10 +0300

    fix checkbox field group

--- tests/CheckboxFieldClick.spec.ts
+++ tests/CheckboxFieldClick.spec.ts
@@ -4,22 +4,18 @@ test.describe('Checkbox Field group', () => {
     await page.goto('https://magstrong07.github.io/admiral-react-test/#/checkbox_field');
   });
   test('Checkbox Field group click 3', async ({ page }) => {
-    await page.check('input[type="checkbox"]');
-    await page.check('text=Не управляемый маленький чекбокс >> input[type="checkbox"]');
-    await page.check(
-      'text=Error не управляемый чекбоксВариация с дополнительно декорированным текстом >> input[type="checkbox"]',
-    );
-    await page.waitForTimeout(250);
+    await page.getByRole('checkbox', { name: 'Управляемый чекбокс' }).check();
+    await page.getByRole('checkbox', { name: 'Не управляемый маленький чекбокс' }).check();
+    await page.getByLabel('Error не управляемый чекбоксВариация с дополнительно декорированным текстом').check();
+    await page.getByLabel('Не управляемый маленький чекбокс indeterminateВариация с дополнительным текстом').check();
     await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
   });
   test('Checkbox Field group click 3 Dark', async ({ page }) => {
-    await page.locator('label[role="switch"] span').click();
-    await page.check('input[type="checkbox"]');
-    await page.check('text=Не управляемый маленький чекбокс >> input[type="checkbox"]');
-    await page.check(
-      'text=Error не управляемый чекбоксВариация с дополнительно декорированным текстом >> input[type="checkbox"]',
-    );
-    await page.waitForTimeout(250);
+    await page.locator('label[role="switch"] span').first().click();
+    await page.getByRole('checkbox', { name: 'Управляемый чекбокс' }).check();
+    await page.getByRole('checkbox', { name: 'Не управляемый маленький чекбокс' }).check();
+    await page.getByLabel('Error не управляемый чекбоксВариация с дополнительно декорированным текстом').check();
+    await page.getByLabel('Не управляемый маленький чекбокс indeterminateВариация с дополнительным текстом').check();
     await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
   });
 });
