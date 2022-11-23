62cae988af1947ce3cf55090e7573800844d9ccf
Andrey Moskvin <moskowkach@gmail.com>
2022-11-23 10:15:23 +0300

    admiral react-ui 3.5

--- tests/multiselect.spec.ts
+++ tests/multiselect.spec.ts
@@ -3,7 +3,14 @@ import { test, expect } from '@playwright/test';
 test('multiselect click', async ({ page }) => {
   await page.goto('https://magstrong07.github.io/admiral-react-test/#/multiselect');
 
-  await page.locator('data-test-id=multiselect >> nth=0').click({ force: true });
+  await page.locator('select >> nth=0').click({ force: true });
   await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
+});
 
+test('multiselect options chips', async ({ page }) => {
+  await page.goto('https://magstrong07.github.io/admiral-react-test/#/multiselect');
+
+  await page.locator('select >> nth=0').click({ force: true }).selectOption(['red', 'green', 'blue']);
+
+  await expect(page).toHaveScreenshot({ fullPage: true, scale: 'css' });
 });
